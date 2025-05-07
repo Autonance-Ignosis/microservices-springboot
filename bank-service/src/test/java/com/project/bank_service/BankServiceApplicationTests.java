4840bd66c88101b7ef6c3c7746a4ddc6168f91a1
Kris0011 <krishp759@gmail.com>
2025-05-08 00:28:00 +0530

    docker files and kubernetes configuration

--- bank-service/src/test/java/com/project/bank_service/BankServiceApplicationTests.java
+++ bank-service/src/test/java/com/project/bank_service/BankServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
